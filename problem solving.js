c9b238dc951fe48db07116c3b9e7418ac244bef8
Arunanakerekanti <131989939+Arunanakerekanti@users.noreply.github.com>
2023-05-09 21:58:36 +0530

    problem solving.js

--- problem solving.js
+++ problem solving.js
@@ -1,4 +1,13 @@
+// sum of numbers using function //
 function numbers(a,b){
     return a+b
 }
 alert(numbers(1,5))
+
+//Convering minutes into seconds using function//
+var minutes =prompt("enter the minute to convert into seconds")
+function minutestoSeconds(minutes){
+    return minutes*60;
+}
+
+alert(minutestoSeconds(minutes))
